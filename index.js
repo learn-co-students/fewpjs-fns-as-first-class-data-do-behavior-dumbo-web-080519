e2d13a1ad07c278509735bcd8c2581e26a365bca
Zermina Ejaz <zerminaejaz1018@gmail.com>
2019-09-16 14:19:33 -0400

    Done.

--- index.js
+++ index.js
@@ -8,4 +8,25 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(time)
+{
+  // console.log("(A)time: " + time)
+  let new_time = time.replace(":", "");
+  // console.log("(B:SPLIT)new_time: " + new_time)
+  new_time = parseInt(new_time, "10");
+  // console.log("(C:PARSE)new_time: " + new_time)
+
+  if(new_time < 1200)
+    return "Good Morning";
+  else if (new_time <= 1700)
+    return "Good Afternoon";
+  else
+    return "Good Evening";
+}
 /* Write your implementation of displayMessage() */
+
+// console.log("Time: 12:00: " + greet("12:00"));
+function displayMessage(msg)
+{
+  document.getElementById("greeting").innerText = msg;
+}
